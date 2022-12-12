8b7b50a445eb38a5b50bcdcbb2453e9afa215fb4
ROHITchin995 <rohitsure.9915@gmail.com>
2022-12-12 19:42:10 +0530

    app grid layout done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import "bootstrap/dist/css/bootstrap.css";
 import './App.css';
+import { Card, CardBody } from 'reactstrap';
+
+
+const singleGrid = new Array(9).fill("empty")
 
 function App() {
   return (
     <div className="App">
-    Testing
+     <div className="grid">
+      
+     {singleGrid.map((grid, index)=>(
+      <Card key={index} color="warning">
+        <CardBody className="box">
+
+        </CardBody>
+        </Card>
+     ))}
+      
+        </div>
     </div>
   );
 }
